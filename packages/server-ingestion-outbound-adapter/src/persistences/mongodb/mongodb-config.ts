9b170d8fabd4ff1e7ac8ee7a60e4716b42cfa411
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-06-13 22:45:25 +0100

    fix: mongodb config url (#52)

--- packages/server-ingestion-outbound-adapter/src/persistences/mongodb/mongodb-config.ts
+++ packages/server-ingestion-outbound-adapter/src/persistences/mongodb/mongodb-config.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const mongodbConfigSchema = z.object({
   INGESTION_MONGODB_URL: z.string(),
-  INGESTION_MONGODB_NAME: z.string(),
+  INGESTION_MONGODB_DATABAES_NAME: z.string(),
 });
 
 const getMongodbConfig = () => mongodbConfigSchema.parse(process.env);
