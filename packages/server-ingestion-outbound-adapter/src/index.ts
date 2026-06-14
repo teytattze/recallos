11510540a2585068c2e5f0edb03baf55559215b6
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-06-14 16:43:30 +0100

    fix name

--- packages/server-ingestion-outbound-adapter/src/index.ts
+++ packages/server-ingestion-outbound-adapter/src/index.ts
@@ -3,4 +3,4 @@ export * from "./cryptography/node-webhook-signature-generator.ts";
 
 export * from "./persistences/mongodb/mongodb-config";
 export * from "./persistences/mongodb/mongodb-unit-of-work";
-export * from "./persistences/mongodb/mongodb-webhook-endpoint-repository.ts";
+export * from "./persistences/mongodb/mongodb-webhook-subscription-repository.ts";
