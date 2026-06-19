119b865962f0a9f66f3c43ee3a309e40079fa1b1
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-06-19 15:01:41 +0100

    rename

--- packages/server-ingestion-outbound-adapter/src/index.ts
+++ packages/server-ingestion-outbound-adapter/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./cryptography/node-webhook-secret-generator.ts";
 export * from "./cryptography/node-webhook-signature-generator.ts";
 
-export * from "./persistences/mongodb/mongodb-config";
-export * from "./persistences/mongodb/mongodb-unit-of-work";
-export * from "./persistences/mongodb/mongodb-webhook-subscription-repository.ts";
+export * from "./persistence/mongodb/mongodb-config.ts";
+export * from "./persistence/mongodb/mongodb-unit-of-work.ts";
+export * from "./persistence/mongodb/mongodb-webhook-subscription-repository.ts";
