4217a2802901a044a5e122aab27648d3f2ac6265
Liam Tat Tze Tey <tattzetey@gmail.com>
2026-06-28 16:02:45 +0100

    move pages

--- apps/web/src/routes/sign-in/route.tsx
+++ apps/web/src/routes/sign-in/route.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { SignInPage } from "@/features/sign-in/sign-in-page";
+import { SignInPage } from "@/pages/sign-in-page";
 
 export const Route = createFileRoute("/sign-in")({
   component: SignInPage,
